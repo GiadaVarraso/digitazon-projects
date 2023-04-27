2558754206e9b092bdcc08cd6e890e49170e3743
giada <giadavarri@gmail.com>
2023-04-27 17:30:47 +0200

    esercizio 2 esame modulo JS completo (pulito codice)

--- esameModuloJS/esameModuloJSes2.js
+++ esameModuloJS/esameModuloJSes2.js
@@ -1,33 +1,15 @@
 //  ESAME MODULO 2 - Javascript
 //  esercizio 2 - Oh, non puoi farci niente. Siamo tutti pazzi qui.
 
-// Scrivere una funzione che sia in grado di dire se una data stringa in ingresso sia o meno
-// “pazza”.
-// Una stringa e’ “pazza” se:
-// ● non ha un soggetto, un soggetto puo’ essere solo: “Lui”, “Lei”, “Egli”, “Ella”
-// ● finisce con “?!?”
-// ● contiene la stringa “Cthulhu”
-// ● inizia e finisce con una parola che finisce in “are”, o “ere”, o “ire”
-// ● inizia con della punteggiatura, quindi con uno tra “,.!?:;-~”
-// Basta uno dei suddetti “requisiti di pazzia” per rendere una frase “pazza”; a meno che la
-// stringa contenga una di queste stringhe, in quel caso la stringa non e’ mai “pazza”:
-// ● Church
-// ● mare
-// Esempi di stringhe pazze:
-// ● .Quando guardi a lungo nell’abisso, l’abisso ti guarda dentro.
-// ● Andare a rimirare
-// Esempi di stringhe non pazze:
-// ● Lui e’ pazzo.
-// ● ~ Pensava sempre al mare come a la mar, come lo chiamano in spagnolo quando lo
-// amano ~
-// ● ~ Papa’, come sta Church? ~
-
 // pseudocodice:
-// devo scrivere una funzione che analizza la stringa che gli viene passata in ingresso e 
-// con una serie di if determina se si verificano o meno le condizioni per cui la stringa viene definita pazza
-// se si verificano ritorna false
-// faccio delle funzioni a parte che si prendono carico ciascuna di fare uno specifico controllo
-// se i controlli vengono superati invece ritorna il booleano true
+// devo scrivere una funzione che analizza la stringa che 
+// gli viene passata in ingresso e 
+// con una serie di if determina se si verificano o meno le condizioni 
+// per cui la stringa viene definita pazza
+// se si verificano ritorna true
+// faccio delle funzioni a parte che si prendono carico ciascuna 
+// di fare uno specifico controllo
+// se i controlli vengono superati invece ritorna il booleano false
 
 function checkSoggetto(str){
     let arSogg=['Lui', 'Lei', 'Egli', 'Ella']
